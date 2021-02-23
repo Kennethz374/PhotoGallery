367ecb1c40ccf2b4df35752b8456c69d615bd2a3
kennethzhang <kennethzhang374@gmail.com>
2021-02-22 22:02:45 -0800

    complete infinite scroll and setting page correctly

--- src/App.js
+++ src/App.js
@@ -3,50 +3,84 @@ import { FaSearch } from "react-icons/fa";
 import Photo from "../src/Photo";
 import Loading from "../src/Loading";
 
-const url = `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
-
 function App() {
 	const [photos, setPhotos] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [page, setPage] = useState(0);
+	const [query, setQuery] = useState("");
+	const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+	const mainUrl = `https://api.unsplash.com/photos/`;
+	const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("I click the button");
+		setPage(1);
 	};
 	const fetchPhotos = async () => {
 		setIsLoading(true);
+		let url;
+		const urlPage = `&page=${page}`;
+		const urlQuery = `&query=${query}`;
+		if (query) {
+			url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+		} else {
+			url = `${mainUrl}${clientID}${urlPage}`;
+		}
 		try {
 			const response = await fetch(`${url}`);
 			const data = await response.json();
-			console.log(data);
 			setIsLoading(false);
-			setPhotos(data);
+			setPhotos((oldPhotos) => {
+				if (query && page === 1) {
+					return data.results;
+				} else if (query) {
+					return [...oldPhotos, ...data.results];
+				} else {
+					return [...oldPhotos, ...data];
+				}
+			});
 		} catch (error) {
 			console.log(error);
+			setIsLoading(false);
 		}
 	};
 
 	useEffect(() => {
 		fetchPhotos();
-	}, []);
-	if (isLoading) {
-		return <Loading />;
-	} else {
-		return (
-			<main>
-				<section className="search">
-					<form className="search-form">
-						<input
-							type="text"
-							className="search-input"
-							placeholder="Search Here"
-						/>
-						<button className="submit-btn" type="submit" onClick={handleSubmit}>
-							<FaSearch />
-						</button>
-					</form>
-				</section>
+	}, [page]);
 
+	useEffect(() => {
+		const event = window.addEventListener("scroll", () => {
+			if (
+				!isLoading &&
+				window.innerHeight + window.scrollY >= document.body.scrollHeight - 5
+			) {
+				setPage((page) => {
+					return page + 1;
+				});
+			}
+		});
+		return window.removeEventListener("scroll", event);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<main>
+			<section className="search">
+				<form className="search-form">
+					<input
+						type="text"
+						className="search-input"
+						placeholder="Search "
+						onChange={(e) => setQuery(e.target.value)}
+						value={query}
+					/>
+					<button className="submit-btn" type="submit" onClick={handleSubmit}>
+						<FaSearch />
+					</button>
+				</form>
+			</section>
+			<>
 				<ul className="photos">
 					{photos.map((photo) => {
 						return <Photo key={photo.id} {...photo} />;
@@ -54,9 +88,10 @@ function App() {
 
 					<li></li>
 				</ul>
-			</main>
-		);
-	}
+				{isLoading && <Loading />}
+			</>
+		</main>
+	);
 }
 
 export default App;
