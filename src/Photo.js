00c170d0c98a5ab38551c1f9245c6fd4f831f2dc
kennethzhang <kennethzhang374@gmail.com>
2021-02-22 21:25:25 -0800

    styling complete

--- src/Photo.js
+++ src/Photo.js
@@ -1,23 +1,26 @@
 import React from "react";
 
-function Photo() {
+function Photo(props) {
+	const {
+		urls: { regular },
+		alt_description,
+		likes,
+		user: {
+			name,
+			portfolio_url,
+			profile_image: { medium },
+		},
+	} = props;
 	return (
 		<li className="photo">
-			<img
-				src="https://images.unsplash.com/photo-1613855233638-8494b9d6247c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNzY2ODF8MHwxfGFsbHwyfHx8fHx8Mnw&ixlib=rb-1.2.1&q=80&w=1080"
-				alt="random img"
-			/>
+			<img src={regular} alt={alt_description} />
 			<div className="img-info">
 				<div className="user">
-					<h4>KEN ZHANG</h4>
-					<p>12 likes</p>
+					<h4>{name}</h4>
+					<p>Likes: {likes}</p>
 				</div>
-				<a href="">
-					<img
-						src="https://images.unsplash.com/photo-1613876654199-92cfed5a07a8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNzY2ODF8MHwxfGFsbHwyNHx8fHx8fDJ8&ixlib=rb-1.2.1&q=80&w=1080"
-						alt="avatar"
-						className="user-avartar"
-					/>
+				<a href={portfolio_url}>
+					<img src={medium} alt="avatar" className="user-avartar" />
 				</a>
 			</div>
 		</li>
---
+++ src/data.js
@@ -0,0 +1,107 @@
+const DUMMY_DATA = {
+	id: "uI-fScjuXnI",
+	created_at: "2021-01-19T17:35:55-05:00",
+	updated_at: "2021-02-22T09:27:34-05:00",
+	promoted_at: null,
+	width: 8063,
+	height: 5375,
+	color: "#d9c0c0",
+	blur_hash: "LPKmnO9w57o#0K9FNJS2JUtRsonN",
+	description: null,
+	alt_description: "A woman looking at a contract on DocuSign from a iPhone",
+	urls: {
+		raw:
+			"https://images.unsplash.com/photo-1611095564350-2cbe940a8d99?ixid=MXwyMDkyMzV8MXwxfGFsbHwxfHx8fHx8Mnw\u0026ixlib=rb-1.2.1",
+		full:
+			"https://images.unsplash.com/photo-1611095564350-2cbe940a8d99?crop=entropy\u0026cs=srgb\u0026fm=jpg\u0026ixid=MXwyMDkyMzV8MXwxfGFsbHwxfHx8fHx8Mnw\u0026ixlib=rb-1.2.1\u0026q=85",
+		regular:
+			"https://images.unsplash.com/photo-1611095564350-2cbe940a8d99?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MXwyMDkyMzV8MXwxfGFsbHwxfHx8fHx8Mnw\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=1080",
+		small:
+			"https://images.unsplash.com/photo-1611095564350-2cbe940a8d99?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MXwyMDkyMzV8MXwxfGFsbHwxfHx8fHx8Mnw\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=400",
+		thumb:
+			"https://images.unsplash.com/photo-1611095564350-2cbe940a8d99?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MXwyMDkyMzV8MXwxfGFsbHwxfHx8fHx8Mnw\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=200",
+	},
+	links: {
+		self: "https://api.unsplash.com/photos/uI-fScjuXnI",
+		html: "https://unsplash.com/photos/uI-fScjuXnI",
+		download: "https://unsplash.com/photos/uI-fScjuXnI/download",
+		download_location: "https://api.unsplash.com/photos/uI-fScjuXnI/download",
+	},
+	categories: [],
+	likes: 37,
+	liked_by_user: false,
+	current_user_collections: [],
+	sponsorship: {
+		impression_urls: [],
+		tagline: "The #1 way to agree",
+		tagline_url: "https://www.docusign.com.au",
+		sponsor: {
+			id: "OaviEFA3-Cs",
+			updated_at: "2021-02-22T16:10:28-05:00",
+			username: "docusign",
+			name: "DocuSign",
+			first_name: "DocuSign",
+			last_name: null,
+			twitter_username: "DocuSign",
+			portfolio_url: "http://www.docusign.com",
+			bio: "The #1 way to agree.",
+			location: null,
+			links: {
+				self: "https://api.unsplash.com/users/docusign",
+				html: "https://unsplash.com/@docusign",
+				photos: "https://api.unsplash.com/users/docusign/photos",
+				likes: "https://api.unsplash.com/users/docusign/likes",
+				portfolio: "https://api.unsplash.com/users/docusign/portfolio",
+				following: "https://api.unsplash.com/users/docusign/following",
+				followers: "https://api.unsplash.com/users/docusign/followers",
+			},
+			profile_image: {
+				small:
+					"https://images.unsplash.com/profile-1605626799058-fc780cc2b8b7image?ixlib=rb-1.2.1\u0026q=80\u0026fm=jpg\u0026crop=faces\u0026cs=tinysrgb\u0026fit=crop\u0026h=32\u0026w=32",
+				medium:
+					"https://images.unsplash.com/profile-1605626799058-fc780cc2b8b7image?ixlib=rb-1.2.1\u0026q=80\u0026fm=jpg\u0026crop=faces\u0026cs=tinysrgb\u0026fit=crop\u0026h=64\u0026w=64",
+				large:
+					"https://images.unsplash.com/profile-1605626799058-fc780cc2b8b7image?ixlib=rb-1.2.1\u0026q=80\u0026fm=jpg\u0026crop=faces\u0026cs=tinysrgb\u0026fit=crop\u0026h=128\u0026w=128",
+			},
+			instagram_username: "docusign",
+			total_collections: 0,
+			total_likes: 1,
+			total_photos: 30,
+			accepted_tos: true,
+		},
+	},
+	user: {
+		id: "OaviEFA3-Cs",
+		updated_at: "2021-02-22T16:10:28-05:00",
+		username: "docusign",
+		name: "DocuSign",
+		first_name: "DocuSign",
+		last_name: null,
+		twitter_username: "DocuSign",
+		portfolio_url: "http://www.docusign.com",
+		bio: "The #1 way to agree.",
+		location: null,
+		links: {
+			self: "https://api.unsplash.com/users/docusign",
+			html: "https://unsplash.com/@docusign",
+			photos: "https://api.unsplash.com/users/docusign/photos",
+			likes: "https://api.unsplash.com/users/docusign/likes",
+			portfolio: "https://api.unsplash.com/users/docusign/portfolio",
+			following: "https://api.unsplash.com/users/docusign/following",
+			followers: "https://api.unsplash.com/users/docusign/followers",
+		},
+		profile_image: {
+			small:
+				"https://images.unsplash.com/profile-1605626799058-fc780cc2b8b7image?ixlib=rb-1.2.1\u0026q=80\u0026fm=jpg\u0026crop=faces\u0026cs=tinysrgb\u0026fit=crop\u0026h=32\u0026w=32",
+			medium:
+				"https://images.unsplash.com/profile-1605626799058-fc780cc2b8b7image?ixlib=rb-1.2.1\u0026q=80\u0026fm=jpg\u0026crop=faces\u0026cs=tinysrgb\u0026fit=crop\u0026h=64\u0026w=64",
+			large:
+				"https://images.unsplash.com/profile-1605626799058-fc780cc2b8b7image?ixlib=rb-1.2.1\u0026q=80\u0026fm=jpg\u0026crop=faces\u0026cs=tinysrgb\u0026fit=crop\u0026h=128\u0026w=128",
+		},
+		instagram_username: "docusign",
+		total_collections: 0,
+		total_likes: 1,
+		total_photos: 30,
+		accepted_tos: true,
+	},
+};
